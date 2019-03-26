17eb5f1e2a6a90689159eee4726f654aa320f2aa
Maratow <marat_17.5@mail.ru>
2019-03-26 14:57:12 +0400

    Add project files.

---
+++ TestForgotPassword/EmailSender.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace TestForgotPassword
+{
+    public class EmailSender : IEmailSender
+    {
+        string host = "smtp.mail.ru";
+        int port = 587;
+        bool enableSSL = true;
+        string userName = "gap.international@bk.ru";
+        string password = "ArmanDanielyan";
+
+        //public EmailSender(string host, int port, bool enableSSL, string userName, string password)
+        //{
+        //    this.host = host;
+        //    this.port = port;
+        //    this.enableSSL = enableSSL;
+        //    this.userName = userName;
+        //    this.password = password;
+        //}
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var client = new SmtpClient(host, port)
+            {
+                Credentials = new NetworkCredential(userName, password),
+                EnableSsl = enableSSL
+            };
+            return client.SendMailAsync(
+                new MailMessage(userName, email, subject, htmlMessage) { IsBodyHtml = true }
+            );
+        }
+    }
+}
---
+++ TestForgotPassword/TokenProviders/PasswordResetTokenProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestForgotPassword.TokenProviders
+{
+    public class PasswordResetTokenProvider<T> : DataProtectorTokenProvider<IdentityUser>
+    {
+        public PasswordResetTokenProvider(IDataProtectionProvider dataProtectionProvider, IOptions<PasswordResetTokenProviderOptions> options) : base(dataProtectionProvider, options)
+        {
+        }
+    }
+
+    public class PasswordResetTokenProviderOptions : DataProtectionTokenProviderOptions
+    {
+
+    }
+}
